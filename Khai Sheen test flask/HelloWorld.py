a3ba9e47cfdefca3fa41a835d135f819d833bf4c
khaisheen <41457502+khaisheen@users.noreply.github.com>
2019-10-20 18:15:19 +0800

    20/10/2019 1815

--- Khai Sheen test flask/HelloWorld.py
+++ Khai Sheen test flask/HelloWorld.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return None
+    return render_template("index.html")
 
+@app.route("/sheen")
+def sheen():
+    return "<h1>COOL</h1> guy!!!"
+
+@app.route("/review")
+def review():
+    return render_template("review-page.html")
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+else:
+    print("Something wrong")
+    
