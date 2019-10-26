4071433c6d93ffe056cdb411579fe72db1e4011d
khaisheen <41457502+khaisheen@users.noreply.github.com>
2019-10-26 15:05:55 +0800

    26/10/19 KS

--- Khai Sheen test flask/HelloWorld.py
+++ Khai Sheen test flask/HelloWorld.py
@@ -1,21 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/sheen")
-def sheen():
-    return "<h1>COOL</h1> guy!!!"
+
+@app.route("/form")
+def form():
+    return render_template("randomForm.html")
+
+
+@app.route("/form", methods=['POST'])
+def get_name():
+    name = request.form['name']
+    print("This idiot's name is", name)
+    return render_template("randomForm.html")
+
 
 @app.route("/review")
 def review():
     return render_template("review-page.html")
-    
+
+
+@app.route("/review", methods=["POST"])
+def submit_review():
+    desc = request.form['reviewDesc']
+    review = request.form['reviewTextInput']
+    return "<strong>" + desc + "</strong>" + review
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 else:
     print("Something wrong")
-    
