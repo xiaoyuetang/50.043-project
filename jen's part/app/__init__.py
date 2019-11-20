e26a3c7911b9b11cb0c0f3d4b57bc5f16b4d3059
jenyangg <ngjenyang@gmail.com>
2019-11-21 03:12:40 +0800

    standalone app with online MySQL
    Completed sync + calls to AWS hosted MySQL db, please do not share for very good reasons.

---
+++ jen's part/app/__init__.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_pymongo import PyMongo
+from flask_mysqldb import MySQL
+
+import logging
+import logging.handlers
+from urllib import parse
+pwd = parse.quote("123456789@")
+ssh_address1='ec2-52-42-232-25.us-west-2.compute.amazonaws.com'
+ssh_address2="ec2-54-227-63-130.compute-1.amazonaws.com"
+app = Flask(__name__, static_url_path='/static')
+app.config.from_object(Config)
+app.config['TESTING'] = True
+logging.basicConfig(level=logging.DEBUG)
+app.config['MYSQL_HOST'] = 'bigdhatta.ceerfqdva59u.ap-southeast-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'J3reakD0wn'
+app.config['MYSQL_DB'] = 'reviews'
+mysql = MySQL(app)
+
+
+
+meta = PyMongo(app,uri="mongodb://books:123456789@"+ssh_address1+":27017/books")
+log = PyMongo(app,uri="mongodb://logadmin:"+pwd+"@"+ssh_address2+":27017/log")
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+
+
+
+from app import routes, models, errors
+
+
+class MongoAlchemyHandler(logging.Handler):
+	def emit(self, record):
+		trace = None
+		exc = record.__dict__['exc_info']
+		if exc:
+			trace = traceback.format_exc() ##CHANGE HERE, removed exc parameter
+		message = record.__dict__['msg']
+		request_index = 0
+		response_index = -2
+		if 'GET' in message:
+			request_index = message.find('GET')
+		if 'POST' in message:
+			request_index = message.find('POST')
+
+		req = message[request_index:-1]
+		if '"' in req and request_index != 0:
+
+			response_index = req.find('"')
+
+		mydict= {
+			"timestamp" : datetime.utcnow(),
+	        "request" : req[0:response_index],
+	        "response" : req[response_index+1:-1]}
+
+		log.db.systemLog.insert(mydict)
+
+		
+
+		
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ch = MongoAlchemyHandler()
+ch.setLevel(logging.INFO)
+
+loggers = [logger, logging.getLogger('werkzeug'), logging.getLogger('flask.app')]
+
+for l in loggers:
+	l.addHandler(ch)
---
+++ jen's part/app/routes.py
@@ -0,0 +1,352 @@
+from app import app
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import LoginForm, ReviewForm, RegistrationForm
+from app import db, log, meta, mysql
+from app.models import User, Review, ReviewerReviews, ReviewerInformation
+from werkzeug.urls import url_parse
+from datetime import datetime
+import MySQLdb
+from mysql.connector import MySQLConnection, Error
+@app.route('/addreview', methods=['GET', 'POST'])
+@login_required
+def addreview():
+	form = ReviewForm()
+	if form.validate_on_submit():
+		reviewID = Trial.query.filter_by(reviewID=form.reviewID.data).first()
+		if reviewID is not None:
+			flash('reviewerID already existed')
+			return redirect(url_for('addreview'))
+
+		reviewID = form.reviewID.data
+		overall = form.overall.data
+		reviewText = form.reviewText.data
+
+		# add review to database
+		review = Trial(reviewID=reviewID, overall=overall, reviewText=reviewText)
+		db.session.add(review)
+		db.session.commit()
+
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
+	return render_template('add_review.html', title='Add a Review', form=form)
+
+####################### below are revisited version ########################
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+	############
+
+	top10_review = Review.query.filter_by(overall=5).limit(35).all()
+	top10_review_asin = []
+	for i in top10_review:
+		reviewerReviews = ReviewerReviews.query.filter_by(reviewID=i.reviewID).first()
+		top10_review_asin.append(reviewerReviews.asin)
+	top10_review_asin = list(dict.fromkeys(top10_review_asin))
+
+	############
+	BookInfoList = []
+	BookList=[]
+	for i in range(len(top10_review_asin)):
+		booki = meta.db.metaKindleStoreClean.find_one({"asin":top10_review_asin[i],'imUrl':{'$exists': True},'description':{'$exists': True},'categories':{'$exists':True}})
+
+		if booki != None and len(BookInfoList) <=10:
+			BookInfoList.append(booki)
+
+	return render_template('index.html',BookInfoList = BookInfoList)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	if request.method == 'POST':
+		if request.form['loginbutton'] == 'Log In':
+
+			userid = request.form['userid']
+			password = request.form['password']
+			user = User.query.filter_by(username=userid).first()
+			if user is None or not user.check_password(password):
+				flash('Invalid username or password')
+				console.log('Invalid username or password')
+				return redirect(url_for('login'))
+			remember = request.form["loginsavepw"]
+			login_user(user, remember=remember)
+			next_page = request.args.get('next')
+			if not next_page or url_parse(next_page).netloc != '':
+				next_page = url_for('index')
+			return redirect(next_page)
+	return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+
+	if request.method == 'POST':
+		if request.form['signupbutton'] == 'Sign up':
+			userid = request.form['new_username']
+			email = request.form['new_email']
+			password = request.form['new_password']
+
+			user = User(username=userid, email=email)
+			user.set_password(password)
+			db.session.add(user)
+			db.session.commit()
+			flash('Congratulations, you are now a registered user!')
+			return redirect(url_for('index'))
+	return render_template('signup.html', title='Register')
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+def insert_review(id,asin,helpful,overall,reviewText,reviewTime,reviewerID,reviewerName,summary,unixReviewTime):
+	query1 = "INSERT INTO reviews(id,asin,helpful,overall,reviewText,reviewTime,reviewerID,reviewerName,summary,unixReviewTime) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+	try:
+		conn = mysql.connection.cursor()		
+		conn.execute(query1,[id,asin,helpful,overall,reviewText,reviewTime,reviewerID,reviewerName,summary,unixReviewTime])
+		mysql.connection.commit()
+		print("Record inserted successfully into reviews table")
+		# conn.close()
+		# mysql.connection.close()
+		print("MySQL connection is closed")
+		return("Record inserted successfully into reviews table")
+	except Error as error:
+	    print(error)
+
+def get_last_id():
+	try:
+		conn = mysql.connection.cursor()
+		conn.execute('SELECT MAX(id) FROM reviews')
+		last_id = conn.fetchone()
+		return last_id
+	except Error as error:
+	    print(error)
+
+@app.route("/review", methods=["POST"])
+@login_required
+def submit_review():
+	submittedreview = request.form
+	if 'reviewbutton' in submittedreview:
+		# return(submittedreview)
+		# return ('submit review clicked')
+		text = request.form['reviewText']
+		summary = request.form['summary']
+		asin = request.form['asin']
+		# store the review into database
+		new_review = Review(reviewID='20', reviewText=text, summary=summary)
+		lastid = get_last_id()
+		print(lastid[0])
+		eyed = lastid[0]+1
+		asin = asin
+		helpful = '-1'
+		overall = 4
+		reviewText = text
+		reviewTime = '-1'
+		reviewerID = '-1'
+		reviewerName = 'jenyang'
+		summary = summary
+		unixReviewTime = '-2'
+		result = insert_review(eyed,asin,helpful,overall,reviewText,reviewTime,reviewerID,reviewerName,summary,unixReviewTime)
+		return render_template("thank-you.html")
+	else:
+		return "Hello"
+
+# save the latest log records to database
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
+
+# edit user profile
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm(current_user.username)
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit_profile'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+def catch_reviews(asin):
+	myresult=[]
+	try:
+		conn = mysql.connection.cursor()		
+		conn.execute("SELECT * FROM reviews where asin = %s",[asin])
+		myresult = conn.fetchall()
+		print("Records for asin " + asin + " fetched from reviews table")
+		# conn.close()
+		# mysql.connection.close()
+		# print("MySQL connection is closed")
+		print(myresult)
+	except Error as error:
+	    print(error)
+	return myresult
+		
+# Description / write a review
+@app.route("/review", methods = ['GET'])
+def review():
+	asin = request.args.get('asin')
+	book = meta.db.metaKindleStoreClean.find_one({"asin":asin})
+	title = book['asin']
+	cover = book['imUrl']
+	desc = book['description']
+	tagslist = book['categories']
+	author = '2333'
+	tags=[]
+	for i in tagslist:
+		for j in i:
+			if j in tags:
+				i.remove(j)
+		tags.extend(i)
+
+	main_book = {"title": title, "cover": cover,
+				 "desc": desc, "author": author, "tags": tags}
+	relateds=[]
+	relatedlist=[]
+
+	if 'also_viewed' in book['related']:
+		relatedlist = book['related']['also_viewed']
+		# relateds = bookinfo(relatedlist)
+	elif book['related']['also_bought'] is not None:
+		relatedlist = book['related']['also_bought']
+		# relateds = bookinfo(relatedlist)
+	elif 'buy_after_viewing' in book['related']:
+		relatedlist = book['related']['buy_after_viewing']
+
+	if len(relatedlist)!=0:
+		relateds = bookinfo(relatedlist)
+
+	#################
+	review_reviews = ReviewerReviews.query.filter_by(asin=asin).limit(10).all()
+	review_ids = []
+	for i in review_reviews:
+		review_ids.append(i.reviewID)
+	records = catch_reviews(asin)
+	# records = Review.query.filter(Review.reviewID.in_(review_ids)).all()
+	# return(str(records))
+	reviews = []
+	for i in records:
+		name = 'Small Bling Bling'
+		img = 'https://media.istockphoto.com/photos/portrait-of-a-smiling-young-woman-picture-id905456806?k=6&m=905456806&s=612x612&w=0&h=PvYHS82wm1FlEh7_8Owj_OamqJfJ8g3igDrfbA4Xo7I='
+		# summary = i.summary
+		# text = i.reviewText
+		# overall = i.overall
+		summary = i[8]
+		text = i[4]
+		overall = i[3]
+
+		review = {"name": name, "img": img, "text": text, "summary": summary, "overall": overall}
+		reviews.append(review)
+	##################
+
+	return render_template("review-page.html", main=main_book, reviews=reviews, relateds=relateds)
+
+def bookinfo(relatedlist):
+	relateds=[]
+	count=0
+	for i in relatedlist:
+		book = meta.db.metaKindleStoreClean.find_one({"asin":i})
+		if book is not None:
+
+			if 'imUrl' in book:
+				title=book['asin']
+				cover=book['imUrl']
+				author='2333'
+				tagslist = book['categories']
+				tags=[]
+				for j in tagslist:
+					for k in j:
+						if k in tags:
+							j.remove(k)
+					tags.extend(j)
+				related = {"cover": cover, "title": title,
+						"author": author, "tags": tags}
+				relateds.append(related)
+				count+=1
+		if count==5:
+			break
+	return relateds
+
+# add-a-book page
+@app.route("/add-a-book")
+@login_required
+def add_a_book():
+	return render_template("add-a-book.html")
+
+# add a new book upon submit
+@app.route("/add-a-book", methods=['POST'])
+@login_required
+def submit_book_info():
+
+	# Get the book info from add-a-book form
+	ClientName = request.form['ClientName']
+	ClientEmail = request.form['ClientEmail']
+	BookCat = request.form["BookCat"]
+	BookName = request.form["BookName"]
+	BookAuthor = request.form["BookAuthor"]
+	MoreAbtBook = request.form["MoreAbtBook"]
+	# Save book information to the mongoDB
+	mydict = {'ClientName':ClientName,
+	'ClientEmail':ClientEmail,
+	'BookCat':BookCat,
+	'BookName':BookName,
+	'BookAuthor':BookAuthor,
+	'MoreAbtBook':MoreAbtBook}
+	meta.db.newbooks.insert(mydict)
+
+	return render_template("thank-you.html")
+
+@app.route("/history")
+@login_required
+def history():
+	'''
+	Using dummy data for now. Fetch from DB next time.
+	'''
+	b1 = "Harry Potter"
+	c1 = "https://img1-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/39885/large_thumb_book-cover-horror-novel-527.jpg"
+	a1 = "A. Dinh"
+	tags1 = ["Art", "Cookbooks"]
+	book1 = {"title": b1, "cover": c1, "author": a1, "tags": tags1}
+
+	b2 = "Rich Dad Poor Dad"
+	c2 = "https://img2-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/37837/large_thumb_stage.jpg"
+	a2 = "B. Dinh"
+	tags2 = ["Self Help", "Thriller", "Graphic Novels"]
+	book2 = {"title": b2, "cover": c2, "author": a2, "tags": tags2}
+
+	b3 = "Lord of The Rings"
+	c3 = "https://i.pinimg.com/236x/82/79/74/827974d98ed5dabfbeecbdae890caebf.jpg"
+	a3 = "C. Dinh"
+	tags3 = ["Business", "Fiction", "Nonfiction"]
+	book3 = {"title": b3, "cover": c3, "author": a3, "tags": tags3}
+
+	books = [book1, book2, book3]
+
+	return render_template("history.html", books=books)
+
+@app.route("/profile")
+# @app.route("/profile/<username>")
+@login_required
+def profile():
+# def profile(username):
+
+	#user = User.query.filter_by(username=username).first_or_404()
+	# 相关的信息需要什么
+	# posts =
+	# reviews =
+
+	return render_template("profile.html")
