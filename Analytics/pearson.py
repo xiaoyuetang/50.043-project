25fcbe06ff6a03738b1709615290683f730bf8c2
khaisheen <41457502+khaisheen@users.noreply.github.com>
2019-12-16 03:20:06 +0800

    Update pearson.py

--- Analytics/pearson.py
+++ Analytics/pearson.py
@@ -1,6 +1,8 @@
 import sys
-import pyspark
-from pyspark import *
+from pyspark import SparkContext, SparkConf
+from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+import numpy as np
 
 sc = SparkContext('local', 'testSpark')
 spark = SparkSession(sc)
@@ -10,10 +12,17 @@ meta = spark.read.json('meta_Kindle_Store.json')
 
 rText = reviews.select('asin','reviewText')
 
+asin_avglength = rText.rdd.reduceByKey(lambda x,y:x+len(y))
+
 price = meta.select('asin','price')
 
-asin_rText_price = rText.join(price).select('*')
+asin_avglength.show(10)
 
-reviewLengths = asin_rText_price.select('reviewText').map(lambda x: len(x))
-
-asin_rText_price = asin_rText_price.join(reviewLengths).select('*')
+#
+#asin_rText_price = rText.join(price).select('*')
+#
+#reviewLengths = asin_rText_price.select('reviewText').rdd.map(lambda x: len(x))
+#
+#asin_rText_price_len = asin_rText_price.join(reviewLengths).select('*')
+#
+#asin_rText_price_len.show(20)
