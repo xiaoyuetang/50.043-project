199b2aa1b978b5b73bf8720a8140b34d53b1203d
khaisheen <41457502+khaisheen@users.noreply.github.com>
2019-12-16 00:13:29 +0800

    Create etl.py

---
+++ Analytics/etl.py
@@ -0,0 +1,23 @@
+import mysql.connector as sqldb
+import os
+import csv
+
+OUTFILE = "out"
+DATABASE = "flaskproject"
+ec2host = 'ec2-34-220-172-38.us-west-2.compute.amazonaws.com'
+# ec2host='54.189.148.212'
+con = sqldb.connect(host=ec2host, user="root", passwd="", db=DATABASE)
+
+
+with open(f"{OUTFILE}", mode="w") as f:
+    cur = con.cursor()
+    cur.execute(
+        "select distinct asin from KindleReview where overall=5 limit 27")
+    data = cur.fetchall()
+    result = []
+    for i in data:
+        result.append(i[0])
+    con.commit()
+
+
+os.system(f'hdfs dfs -put {OUTFILE} test')
