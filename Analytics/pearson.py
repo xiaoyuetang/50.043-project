deb283438136bc1cee1a9bbdfb5a46cc76802937
khaisheen <41457502+khaisheen@users.noreply.github.com>
2019-12-20 02:48:35 +0800

    Update pearson.py

--- Analytics/pearson.py
+++ Analytics/pearson.py
@@ -2,16 +2,29 @@ import pyspark
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as fns
 
-hdfs_addr="hdfs://ec2-54-162-120-120.compute-1.amazonaws.com:9000/user/ubuntu"
+hdfs_addr="hdfs://ec2-54-208-153-234.compute-1.amazonaws.com:9000/user/ubuntu"
 
-sc = pyspark.SparkContext("spark://ec2-54-162-120-120.compute-1.amazonaws.com:7077", "Correlation")
+ sc = pyspark.SparkContext("spark://ec2-54-208-153-234.compute-1.amazonaws.com:7077", "Correlation")
+#sc = pyspark.SparkContext("local", "Correlation")
 spark = SparkSession(sc)
 
 reviews = spark.read.csv(f"{hdfs_addr}/datasets/kindle_reviews.csv", header=True)
+#reviews = spark.read.csv(f"kindle_reviews.csv", header=True)
 asinr = reviews.select('asin','reviewText')
 asinrl = asinr.withColumn('reviewLength',fns.length('reviewText'))
-meta = spark.read.json(f"{hdfs_addr}/datasets/meta_Kindle_Store.json")
 
+meta = spark.read.json(f"{hdfs_addr}/datasets/meta_Kindle_Store.json")
+#meta = spark.read.json("meta_Kindle_Store.json")
+
+# asin_avgl = asinrl.groupBy('asin').avg('reviewLength')
 asin_avgl = asinrl.groupBy('asin').avg('reviewLength')
 
-asin_avgl.take(5)
+#asin_avgl.take(5)
+
+# join asin_avgl and meta with column asin
+table = asin_avgl.join(meta.select('asin', 'price'), ['asin'])
+
+# return Column for the Pearson Correlation Coefficient 
+table = table.agg(fns.corr('avg(reviewLength)', "price").alias('pearson_correlation'))
+
+table.select("pearson_correlation").show()
