8c6c01438740bd42ed09927ed372459cf6c0f193
keelia <xiaoyuetang2017@gmail.com>
2019-12-22 23:45:45 +0800

    -- stupid bug ---

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -1,3 +1,4 @@
+from app import routes, models, errors
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -13,11 +14,11 @@ import os
 
 ####for automation script######
 ################
- file = open('ip.txt','r')
- for line in file:
-     ips=line.split(' ')
-     mysqlIp=ips[0]
-     metaIp=ips[1]
+file = open('ip.txt', 'r')
+for line in file:
+    ips = line.split(' ')
+    mysqlIp = ips[0]
+    metaIp = ips[1]
 ################
 
 
@@ -28,16 +29,16 @@ app.config.from_object(Config)
 
 ######################################################################
 # con= sqldb.connect(host=mysqlIp, user="root", passwd="", db="flaskproject")
-meta = PyMongo(app,uri="mongodb://books:123456789@"+metaIp+":27017/books")
+meta = PyMongo(app, uri="mongodb://books:123456789@"+metaIp+":27017/books")
 #########################################################################
-ec2host='35.160.25.169'
-ssh_address1='34.217.109.132'
+ec2host = '35.160.25.169'
+ssh_address1 = '34.217.109.132'
 
-app.config['MYSQL_HOST']=mysqlIp
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='flaskproject'
-mysql=MySQL(app)
+app.config['MYSQL_HOST'] = mysqlIp
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskproject'
+mysql = MySQL(app)
 #con = mysql.connection.cursor()
 #con= sqldb.connect(host=ec2host, user="root", passwd="", db="flaskproject")
 #meta = PyMongo(app,uri="mongodb://books:123456789@"+ssh_address1+":27017/books")
@@ -49,8 +50,6 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models, errors
 
 
 # class MongoAlchemyHandler(logging.Handler):
@@ -78,10 +77,6 @@ from app import routes, models, errors
 # 	        "response" : req[response_index+1:-1]}
 
 # 		log.db.systemLog.insert(mydict)
-
-
-
-
 
 
 # logger = logging.getLogger(__name__)
