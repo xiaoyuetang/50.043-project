4476df1ad5e905c26a938581a57b4ed2cf934fe1
keelia <xiaoyuetang2017@gmail.com>
2019-12-23 01:22:34 +0800

    --- PERFECT ---

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -1,4 +1,3 @@
-from app import routes, models, errors
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +11,10 @@ import logging
 import logging.handlers
 import os
 
+app = Flask(__name__, static_url_path='/static')
+app.config.from_object(Config)
+
+
 ####for automation script######
 ################
 file = open('ip.txt', 'r')
@@ -23,9 +26,6 @@ for line in file:
 
 
 #################
-
-app = Flask(__name__, static_url_path='/static')
-app.config.from_object(Config)
 
 ######################################################################
 # con= sqldb.connect(host=mysqlIp, user="root", passwd="", db="flaskproject")
@@ -50,45 +50,4 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
-
-
-# class MongoAlchemyHandler(logging.Handler):
-# 	def emit(self, record):
-# 		trace = None
-# 		exc = record.__dict__['exc_info']
-# 		if exc:
-# 			trace = traceback.format_exc() ##CHANGE HERE, removed exc parameter
-# 		message = record.__dict__['msg']
-# 		request_index = 0
-# 		response_index = -2
-# 		if 'GET' in message:
-# 			request_index = message.find('GET')
-# 		if 'POST' in message:
-# 			request_index = message.find('POST')
-
-# 		req = message[request_index:-1]
-# 		if '"' in req and request_index != 0:
-
-# 			response_index = req.find('"')
-
-# 		mydict= {
-# 			"timestamp" : datetime.utcnow(),
-# 	        "request" : req[0:response_index],
-# 	        "response" : req[response_index+1:-1]}
-
-# 		log.db.systemLog.insert(mydict)
-
-
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-
-# ch = MongoAlchemyHandler()
-# ch.setLevel(logging.INFO)
-
-# loggers = [logger, logging.getLogger('werkzeug'), logging.getLogger('flask.app')]
-
-# for l in loggers:
-# 	l.addHandler(ch)
-
-# if __name__ == "__main__":
-# 	app.run(host="0.0.0.0", port=80)
+from app import routes, models, errors
